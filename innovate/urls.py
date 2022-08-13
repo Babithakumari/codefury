56066fee523317f9cb414070bba7014c1d38cba0
Babitha_Kumari <babithakumari120@gmail.com>
2022-08-13 17:09:52 +0530

    added search feature

--- innovate/urls.py
+++ innovate/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("create", views.create, name="create"),
     path("edit/<int:s_id>",views.edit, name="edit"),
     path("delete/<int:s_id>", views.delete,name="delete"),
-
+    path("search", views.search, name="search"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
